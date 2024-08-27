6391959e5178d1af10af0a4853378bb5a8af87f3
JhRamirez <150725448+Focus0Wizard@users.noreply.github.com>
2024-08-27 17:41:08 -0400

    update of the led class for the function blink

--- Practice1/Led.h
+++ Practice1/Led.h
@@ -1,18 +1,48 @@
 class LED {
   private:
-    int pin;
+    const unsigned char OFF = 0;
+    const unsigned char ON = 1;
+    unsigned char pin;
+    unsigned int interval;
+    unsigned long previousMillis;
+    unsigned char state;
+    unsigned char isBlinking;
 
   public:
-    LED(int pin) {
+    LED(unsigned char pin) {
       this->pin = pin;
+      previousMillis = 0;
       pinMode(pin, OUTPUT);
-    }
-
-    void on() {
-      digitalWrite(pin, HIGH);
+      off();
     }
 
     void off() {
-      digitalWrite(pin, LOW);
+      state = OFF;
+      isBlinking = 0;
+      digitalWrite(pin, state);
+    }
+
+    void on() {
+      state = ON;
+      isBlinking = 0;
+      digitalWrite(pin, state);
+    }
+
+    void blink(unsigned int interval) {
+      this->interval = interval;
+      isBlinking = 1;
+      state = OFF;
+      digitalWrite(pin, state);
+    }
+
+    void touch() {
+      if (isBlinking) {
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= interval) {
+          previousMillis = currentMillis;
+          state = (state == OFF) ? ON : OFF;
+          digitalWrite(pin, state);
+        }
+      }
     }
 };
--- Practice1/Practice1.ino
+++ Practice1/Practice1.ino
@@ -1,22 +1,24 @@
-#include <UltrasonicSensor.h>
-#include <Led.h>
 #define TRIG_PIN 14
 #define ECHO_PIN 12
 #define MAX_DISTANCE 30
 
+// Crear instancia del sensor de ultrasonido
 UltrasonicSensor sensor(TRIG_PIN, ECHO_PIN);
+
+// Crear array de instancias de LEDs
 LED leds[] = { LED(23), LED(22), LED(18), LED(19), LED(21) };
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  // Inicializar la comunicación serial
 }
 
 void loop() {
   long distance = sensor.getDistance();
-  int ledCount;
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
+  int ledCount;
+
   if (distance <= 0) {
     ledCount = 6;
   } else if (distance >= MAX_DISTANCE) {
@@ -27,11 +29,11 @@ void loop() {
 
   for (int i = 0; i < 6; i++) {
     if (i < ledCount) {
-      leds[i].on();
+      leds[i].on(); 
     } else {
       leds[i].off();
     }
+    leds[i].touch();
   }
-  
-  delay(100);
+
 }
